1a11532490c248c19c1ca4bb26e9efea2cc1ae77
Jon Peterson <jon@pnwebs.com>
2019-01-24 12:53:32 -0700

    Make answers accept guesses with incorrect punctuation or capitalization

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,11 @@
 class Turn
   attr_reader :guess, :card
   def initialize(string, card)
-    @guess = string
+    @guess = string.gsub(/[\W]+/,"").downcase
     @card = card
   end
   def correct?
-    return @guess == @card.answer
+    return @guess == @card.answer.gsub(/[\W]+/,"").downcase
   end
   def feedback
     return self.correct? ? "Correct!" : "Incorrect."
