2a4e7beeb2ee01affb875d8e822b25a797087365
Jon Peterson <jon@pnwebs.com>
2019-01-24 09:24:29 -0700

    Add functionality to limit game to a single category

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -9,11 +9,44 @@ cards << Card.new("What video game series stars a Runner named Faith?", "Mirror'
 cards << Card.new("What is the largest continent on Earth by area?", "Asia", :Geography)
 cards << Card.new("What is the airspeed velocity of an unladen swallow?", "African or European?", :Entertainment)
 cards << Card.new("What progamming language are we using in this Mod?", "Ruby", :Technology)
+cards << Card.new("What genre of music is the artist 'Skrillex' best known for?", "Dubstep", :Entertainment)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+# cards << Card.new("", "", :)
+cards.shuffle!
 
 deck = Deck.new(cards)
 round = Round.new(deck)
 
 def start(round)
+  puts "Hello! Before we begin, do you want to choose a category?"
+  puts "If not, we will play with all available cards. Please enter 'y' or 'n'"
+  if gets.chomp == "y"
+    available_categories = round.deck.list_categories
+    selected_category = ""
+    while !available_categories.include?(selected_category)
+      puts "Which category would you like to play?"
+      puts available_categories.join(" | ")
+      selected_category = gets.chomp.to_sym
+      if !available_categories.include?(selected_category)
+        puts "There are no cards in that category."
+      end
+    end
+    filtered_deck = Deck.new(round.deck.cards_in_category(selected_category))
+    round = Round.new(filtered_deck)
+  end
+
   puts "Welcome! You're playing with #{round.deck.count} cards."
   puts "--------------------------------------------------------------"
   while round.turns.length < round.deck.count do
